4bb1003d691c02f07f4e3fd0846fa3ccb2fcc922
ARTHUR DE OLIVEIRA ABRAO <arthur.abrao@aluno.senac.rio>
2026-08-24 21:42:33 -0300

    versionamento2408226

---
+++ UC01/aula06/aula24082026.py
@@ -0,0 +1,62 @@
+#Aula 24 de agosto de 2026
+
+#Lista
+# [] list
+# () tuple
+# {} Set
+
+# lista_01 = [12,
+#             "pedro",
+#             False,
+#             [2,4,5,6]
+#             ]
+# print(lista_01[1],lista_01[3][3])
+
+#condicional
+
+# lista_02 = ["Arthur"]
+
+# if "Arthur" in lista_02:
+#     print(lista_02)
+
+#lopping
+
+# participantes = ["Arthur", "Luana", "Fernando"]
+# # for participante in participantes:
+# #     print(participante)
+
+# partc_02 = "Hugo"
+
+# # participantes.append(partc_02)
+# participantes.insert(2,partc_02)
+
+
+
+
+
+# print(participantes)
+
+#TUPLAS ()
+
+# participantes = ("Arthur", "Luana", "Fernando")
+
+#SETS:{}
+
+# numeros_pares = {
+#     202,
+#     203,
+#     204,
+#     200,
+# }
+
+# print(numeros_pares,type(numeros_pares))
+
+# DICT {key:value}
+
+produto = {
+    "maçã":5.99,"laranja": 4.79
+}
+
+print(produto,(type(produto)))
+
+produto[]
